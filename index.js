548ac8c14633ea587d7ca4268b95ae464e05b25e
Raj kc <157774717+rajkc45@users.noreply.github.com>
2026-07-10 14:45:32 +0545

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app=express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
 const PORT=3000;  
 const students=[
     {id:1,name:"John Doe",age:20},
